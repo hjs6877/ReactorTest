43550c81cd040d2d56cfa5d36cbe77fdf8e4b8c3
jshwang <jshwang@pentasecurity.com>
2020-08-31 18:00:50 +0900

    Add Any operation

--- src/test/java/com/itvillage/springinaction/Chapter10AnyTest.java
+++ src/test/java/com/itvillage/springinaction/Chapter10AnyTest.java
@@ -1,5 +1,24 @@
 package com.itvillage.springinaction;
 
-public class Chapter10AnyTest {
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+public class Chapter10AnyTest {
+    @Test
+    public void anyTest() {
+        Flux<String> flux = Flux
+                .just("elephant", "koala", "eagle", "kangaroo");
+
+        Mono<Boolean> hasTMono = flux.any(animal -> animal.contains("t"));
+        StepVerifier.create(hasTMono)
+                .expectNext(true)
+                .verifyComplete();
+
+        Mono<Boolean> hasZMono = flux.any(animal -> animal.contains("z"));
+        StepVerifier.create(hasZMono)
+                .expectNext(false)
+                .verifyComplete();
+    }
 }
